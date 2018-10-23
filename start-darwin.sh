f3da3e7f38475008f9211acce15f9d6ad56e68b2
Robert Lestak <robert.lestak@umusic.com>
2018-10-23 07:53:01 -0700

    Separate build and start scripts

--- start-darwin.sh
+++ start-darwin.sh
@@ -1,8 +1,8 @@
-docker build . -t jenkinsdock
-
+#!/bin/bash
 docker run -d -p 8080:8080 \
        -p 5000:5000 \
        -v jenkins:/var/jenkins_home \
        -v /var/run/docker.sock:/var/run/docker.sock \
+       --name jenkinsdock \
        --restart always \
        --group-add staff jenkinsdock
--- start-ubuntu.sh
+++ start-ubuntu.sh
@@ -1,9 +1,10 @@
-docker build . -t jenkinsdock
+#!/bin/bash
 
 docker run -d -p 8080:8080 \
        -p 5000:5000 \
        -v jenkins:/var/jenkins_home \
        -v /var/run/docker.sock:/var/run/docker.sock \
        --restart always \
+       --name jenkinsdock \
        --group-add 1000 --group-add 999 \
       jenkinsdock
