b2dc49af0af6111c99bb3e8d006413fa492df02a
Robert Lestak <robert.lestak@umusic.com>
2019-09-16 23:21:24 -0700

    Dynamic host docker group

--- start-linux.sh
+++ start-linux.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+DOCKER_GROUP=$(grep 'docker' /etc/group | awk -F':' '{print $3}')
+if [[ -z "$DOCKER_GROUP" ]]; then
+  echo docker not found in /etc/group > /dev/stderr
+  exit 1
+fi
 
 docker run -d -p 8080:8080 \
        -p 5000:5000 \
@@ -6,5 +11,5 @@ docker run -d -p 8080:8080 \
        -v /var/run/docker.sock:/var/run/docker.sock \
        --restart always \
        --name jenkinsdock \
-       --group-add 1000 --group-add 999 \
+       --group-add $DOCKER_GROUP \
       jenkinsdock
